f6166668a2a5a748c9c80bafc48e97b1a3b50c95
dannygurman <danny.gurman@gmail.com>
2021-02-15 09:52:09 +0200

    spring elasticearch5

---
+++ src/test/java/spring/examples/elasticsearch/tests/SpringContextManualTest.java
@@ -0,0 +1,22 @@
+package spring.examples.elasticsearch.tests;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import spring.examples.elasticsearch.config.ElasticsearchConfig;
+
+/**
+ * This Manual test requires: Elasticsearch instance running on localhost:9200.
+ *
+ * The following docker command can be used: docker run -d --name es762 -p
+ * 9200:9200 -e "discovery.type=single-node" elasticsearch:7.6.2
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = ElasticsearchConfig.class)
+public class SpringContextManualTest {
+
+    @Test
+    public void whenSpringContextIsBootstrapped_thenNoExceptions() {
+    }
+}
