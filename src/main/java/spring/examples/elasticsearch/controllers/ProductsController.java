0f6d3ed2e53444912c76ef61749fd3e5b0170b61
dannygurman <danny.gurman@gmail.com>
2021-02-18 18:29:46 +0200

    spring elasticearch6

--- src/main/java/spring/examples/elasticsearch/controllers/ProductsController.java
+++ src/main/java/spring/examples/elasticsearch/controllers/ProductsController.java
@@ -5,7 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 import spring.examples.elasticsearch.model.Product;
-import spring.examples.elasticsearch.services.ProductService;
+import spring.examples.elasticsearch.services.ProductServiceWithRepository;
+import spring.examples.elasticsearch.services.ProductServiceWithRepository;
 
 import java.util.List;
 import java.util.Optional;
@@ -16,7 +17,7 @@ import java.util.Optional;
 public class ProductsController {
 
     @Autowired
-    private ProductService productService;
+    private ProductServiceWithRepository productServiceWithRepo;
 
     //-------------------------
     /*
@@ -27,20 +28,20 @@ public class ProductsController {
         Note - id field optional*/
     @PostMapping
     public Product save(@RequestBody Product product) {
-        return productService.save(product);
+        return productServiceWithRepo.save(product);
     }
 
     //------------------------------
     //DELETE http://127.0.0.1:9999/products/
     @DeleteMapping
     public void deleteAll() {
-        productService.deleteAll();
+        productServiceWithRepo.deleteAll();
     }
 
     //------------------------------------
     @GetMapping()
     public Page<Product>  findAll() {
-        Page<Product> products = productService.findAll();
+        Page<Product> products = productServiceWithRepo.findAll();
         return products;
     }
 
@@ -48,11 +49,11 @@ public class ProductsController {
     //Call example - http://127.0.0.1:9999/products/1
     @GetMapping(path = "{id}")
     public Optional<Product> findById(@PathVariable(value = "id") String id) {
-        return productService.findById(id);
+        return productServiceWithRepo.findById(id);
     }
 
     @GetMapping(path = "/byname/{name}")
     public List<Product> findByName(@PathVariable(value = "name") String name) {
-        return productService.findByName(name);
+        return productServiceWithRepo.findByName(name);
     }
 }
--- src/main/java/spring/examples/elasticsearch/services/ProductServiceWithRepository.java
+++ src/main/java/spring/examples/elasticsearch/services/ProductServiceWithRepository.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-public class ProductService {
+public class ProductServiceWithRepository {
 
     @Autowired
     private ProductRepository productRepository;
