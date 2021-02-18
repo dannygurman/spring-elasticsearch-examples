5a110eb4642b0c46684d9484f20eb05ed6b1ddb9
dannygurman <danny.gurman@gmail.com>
2021-02-18 19:58:27 +0200

    spring elasticearch8

--- src/main/java/spring/examples/elasticsearch/controllers/ProductsV2Controller.java
+++ src/main/java/spring/examples/elasticsearch/controllers/ProductsV2Controller.java
@@ -2,9 +2,15 @@ package spring.examples.elasticsearch.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import spring.examples.elasticsearch.services.ProductServiceWithESRestTemplate;
 
 //Call example - http://127.0.0.1:9999/products/v2/1
 @RestController
 @RequestMapping(value = "/products/v2")
 public class ProductsV2Controller {
+
+    private ProductServiceWithESRestTemplate productServiceWithESRestTemplate;
+
+
+
 }
---
+++ src/main/java/spring/examples/elasticsearch/services/ProductServiceWithESRestTemplate.java
@@ -0,0 +1,50 @@
+package spring.examples.elasticsearch.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.mapping.IndexCoordinates;
+import org.springframework.data.elasticsearch.core.query.IndexQuery;
+import org.springframework.data.elasticsearch.core.query.IndexQueryBuilder;
+import org.springframework.stereotype.Service;
+import spring.examples.elasticsearch.config.IndexConsts;
+import spring.examples.elasticsearch.model.Product;
+
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import static spring.examples.elasticsearch.config.IndexConsts.PRODUCTS_INDEX_NAME;
+
+@Service
+public class ProductServiceWithESRestTemplate {
+
+    // ElasticsearchRestTemplate implements the interface ElasticsearchOperations,
+    // which does the heavy lifting for low-level search and cluster actions.
+
+    private ElasticsearchOperations elasticsearchOperations;
+
+    @Autowired
+    public ProductServiceWithESRestTemplate(final ElasticsearchOperations elasticsearchOperations) {
+        super();
+        this.elasticsearchOperations = elasticsearchOperations;
+    }
+
+
+    public String createProductIndex(Product product) {
+
+        IndexQuery indexQuery = new IndexQueryBuilder().withId(product.getId()).withObject(product).build();
+        String documentId = elasticsearchOperations.index(indexQuery, IndexCoordinates.of(PRODUCTS_INDEX_NAME));
+
+        return documentId;
+    }
+
+    public List<String> createProductIndexBulk(final List<Product> products) {
+        final Function <Product,IndexQuery> productToIndexQueryMapper = product -> new IndexQueryBuilder().
+                withId(product.getId())
+                .withObject(product).build();
+
+        List<IndexQuery> queries = products.stream().map(productToIndexQueryMapper).collect(Collectors.toList());
+        return elasticsearchOperations.bulkIndex(queries, IndexCoordinates.of(PRODUCTS_INDEX_NAME));
+
+    }
+}
