f84c4da1aca567e990799b61fdcbb4541623951d
dannygurman <danny.gurman@gmail.com>
2021-05-20 22:22:25 +0300

    spring elasticearch31

--- src/main/java/spring/examples/elasticsearch/config/ElasticsearchConfig.java
+++ src/main/java/spring/examples/elasticsearch/config/ElasticsearchConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.RestClients;
+import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
@@ -13,10 +14,11 @@ import org.springframework.data.elasticsearch.repository.config.EnableElasticsea
 @Configuration
 @EnableElasticsearchRepositories(basePackages = "com.baeldung.spring.data.es.repository")
 @ComponentScan(basePackages = { "com.baeldung.spring.data.es.service" })
-public class ElasticsearchConfig {
+public class ElasticsearchConfig extends AbstractElasticsearchConfiguration {
 
+    @Override
     @Bean
-    RestHighLevelClient client() {
+    public RestHighLevelClient elasticsearchClient() {
         ClientConfiguration clientConfiguration = ClientConfiguration.builder()
                 .connectedTo("localhost:9200")
                 .build();
@@ -25,9 +27,9 @@ public class ElasticsearchConfig {
                 .rest();
     }
 
-    @Bean
+    /*@Bean
     public ElasticsearchOperations elasticsearchTemplate() {
-        return new ElasticsearchRestTemplate(client());
-    }
+        return new ElasticsearchRestTemplate(elasticsearchClient());
+    }*/
 
 }
